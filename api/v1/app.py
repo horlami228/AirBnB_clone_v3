e83a33f7db830ec53c5fd880bfe26bab6e96d653
Olamilekan <akintolaolamilekan51@gmail.com>
2023-11-04 22:43:10 +0100

    error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """Flask application"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
@@ -16,6 +16,11 @@ app.register_blueprint(app_views)
 def shutdown_session(exception=None):
     """perform cleanup when app is shutdown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found_error(err):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
